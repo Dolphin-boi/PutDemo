2c69c737502a5cc31922e427bd795aed83ac191f
xsolagul <mark_kuuga@hotmail.com>
2023-01-13 16:21:34 +0700

    add filter in get all

--- server/api/coilApi.js
+++ server/api/coilApi.js
@@ -1,20 +1,36 @@
-const express = require("express")
-const router = express.Router()
-const {errorRes,successRes} = require('../common/response')
-const config = require('config')
-const dbConfig = config.get('dbConfig.db_Name')
-const db = require('../db');
-const {coil,metalType,vendor} = db
+const express = require("express");
+const router = express.Router();
+const { errorRes, successRes } = require("../common/response");
+const config = require("config");
+const dbConfig = config.get("dbConfig.db_Name");
+const db = require("../db");
+const { coil, metalType, vendor } = db;
 
-router
-    .get("/",async (req,res) => {
-        const data = await coil.findAll({include:[
-            { model:metalType , 
-             // attributes: ['name','vendorID'] , 
-              include:[ {model:vendor} ] 
-            }
-          ]})
-        successRes(res,data)
-    })
+router.get("/", async (req, res) => {
+  const filterData = req.query;
+  const filterObj = {};
+  if (filterData.status) {
+    filterObj.statusID = filterData.status;
+  }
+  // if(filterData.vendor){
+  //     filterObj.vendorID = filterData.vendor
+  // }
 
-module.exports = router; 
+  if (filterData.metalType) {
+    console.log(filterData.metalType)
+    filterObj.typeID = filterData.metalType;
+    console.log(filterObj.typeID)
+  }
+  const data = await coil.findAll({
+    include: [
+      {
+        model: metalType,
+        // attributes: ['name','vendorID'] ,
+        include: [{ model: vendor }],
+      },
+    ],where: {...filterObj}
+  });
+  successRes(res, data);
+});
+
+module.exports = router;
